da58637ea54a37e5f44edb0f237bc088074b383c
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-04-02 14:28:19 +0300

    brand added

---
+++ DemoProject/src/demoProject/Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
@@ -0,0 +1,47 @@
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using NArchitecture.Core.Application.Pipelines.Caching;
+using NArchitecture.Core.Application.Requests;
+using NArchitecture.Core.Application.Responses;
+using NArchitecture.Core.Persistence.Paging;
+using static Application.Features.Brands.Constants.BrandsOperationClaims;
+
+namespace Application.Features.Brands.Queries.GetList;
+
+public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDto>>, ICachableRequest  //ISecuredRequest var burada normalde ama yetkili kişi olmadan işlem yapamayız o zaman. Bu anlamda önemli bir interface
+{
+    public PageRequest PageRequest { get; set; }
+
+    public string[] Roles => [Admin, Read];
+
+    public bool BypassCache { get; }
+    public string? CacheKey => $"GetListBrands({PageRequest.PageIndex},{PageRequest.PageSize})";
+    public string? CacheGroupKey => "GetBrands";
+    public TimeSpan? SlidingExpiration { get; }
+
+    public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, GetListResponse<GetListBrandListItemDto>>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public GetListBrandQueryHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetListResponse<GetListBrandListItemDto>> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+        {
+            IPaginate<Brand> brands = await _brandRepository.GetListAsync(
+                index: request.PageRequest.PageIndex,
+                size: request.PageRequest.PageSize,
+                cancellationToken: cancellationToken
+            );
+
+            GetListResponse<GetListBrandListItemDto> response = _mapper.Map<GetListResponse<GetListBrandListItemDto>>(brands);
+            return response;
+        }
+    }
+}
---
+++ DemoProject/src/demoProject/Domain/Entities/Brand.cs
@@ -0,0 +1,7 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+public class Brand : Entity<Guid>
+{
+    public string Name { get; set; }
+}
