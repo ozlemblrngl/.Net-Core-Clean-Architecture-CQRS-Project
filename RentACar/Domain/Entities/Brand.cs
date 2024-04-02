b4fdcea61c4f9cf3133b4f52d3604fd6bedd332f
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-04-02 15:25:03 +0300

    new project added

---
+++ RentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,25 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand : Entity<Guid>
+{
+
+    public string Name { get; set; }
+
+    //kullanım kolaylığı sağlamak üzere aşağıdaki constructor ları yapıyoruz
+
+    public Brand()
+    {
+
+    }
+
+    public Brand(Guid id, string name)
+    {
+        Id = id;
+
+        Name = name;
+    }
+
+
+}
