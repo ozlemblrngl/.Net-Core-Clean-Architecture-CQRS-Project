58361dbf6fe14e557df37f2370e64f86667d4871
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-04-02 15:34:22 +0300

    unnecessary classes deleted

--- RentACar/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ RentACar/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da58637ea54a37e5f44edb0f237bc088074b383c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4fdcea61c4f9cf3133b4f52d3604fd6bedd332f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
