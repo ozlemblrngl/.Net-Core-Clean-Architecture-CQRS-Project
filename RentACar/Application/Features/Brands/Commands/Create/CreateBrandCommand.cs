fb77e37435053e834490b0cf630b8035e87474f4
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-04-06 23:23:12 +0300

    handle method for createBrandCommand was edited

--- RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Application.Services.Repositories;
+using Domain.Entities;
+using MediatR;
 
 namespace Application.Features.Brands.Commands.Create
 {
@@ -17,12 +19,24 @@ namespace Application.Features.Brands.Commands.Create
         // aşağıdaki interface implementasyon yapmamızı bekliyor.
         public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
         {
-            public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            private readonly IBrandRepository _brandRepository;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository)
             {
-                CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
-                createdBrandResponse.Name = request.Name;
-                createdBrandResponse.Id = new Guid();
-                return null;
+                _brandRepository = brandRepository;
+            }
+            public async Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                Brand brand = new();
+                brand.Name = request.Name;
+                brand.Id = Guid.NewGuid();
+
+                var result = await _brandRepository.AddAsync(brand);
+
+                CreatedBrandResponse createdBrandResponse = new();
+                createdBrandResponse.Id = result.Id;
+                createdBrandResponse.Name = result.Name;
+                return createdBrandResponse;
             }
         }
     }
